429873931ab8515c39772898a8b7403bbc80111a
gaoyong <gaoyong06@qq.com>
2023-05-18 17:06:09 +0800

    修改：调用方式和返回值

--- detect_image_gender.py
+++ detect_image_gender.py
@@ -2,13 +2,17 @@
 Author: gaoyong gaoyong06@qq.com
 Date: 2023-05-18 16:10:01
 LastEditors: gaoyong gaoyong06@qq.com
-LastEditTime: 2023-05-18 16:39:29
+LastEditTime: 2023-05-18 17:05:13
 FilePath: \Gender-and-Age-Detection\detect_image_gender.py
 Description: 对detect.py做了一些裁剪,去掉了摄像头的处理,去掉了关于绘制边框和文字的部分，只保留图片内人脸识别及性别检测,如果识别为男性则打印Male,如果识别为女性则打印Female,如果为检测到人脸则打印No face detected
 '''
 import sys
 import cv2
 import argparse
+import json
+
+# 使用示例：python detect_image_gender.py --image D:/work/images/detect_gender/25.jpeg D:/work/images/detect_gender/26.jpeg D:/work/images/detect_gender/27.jpeg
+# 返回值：[{"image": "D:/work/images/detect_gender/25.jpeg", "status": 0, "message": "", "gender": ["Male", "Female"]}, {"image": "D:/work/images/detect_gender/26.jpeg", "status": 0, "message": "", "gender": ["Male", "Male", "Male"]}, {"image": "D:/work/images/detect_gender/27.jpeg", "status": 0, "message": "", "gender": ["Female", "Female"]}]
 
 # 设置中文编码
 sys.stdout.reconfigure(encoding='utf-8')
@@ -27,7 +31,7 @@ def highlightFace(net, frame, conf_threshold=0.7):
     faceBoxes=[]
     for i in range(detections.shape[2]):
         confidence=detections[0,0,i,2]
-        # 根据阈值thresh筛选出置信度大于阈值的人脸框
+        # 根据阈值thresh筛选出置信度大于阈值的人脸
         if confidence>conf_threshold:
             x1=int(detections[0,0,i,3]*frameWidth)
             y1=int(detections[0,0,i,4]*frameHeight)
@@ -38,7 +42,7 @@ def highlightFace(net, frame, conf_threshold=0.7):
 
 # 解析命令行参数
 parser=argparse.ArgumentParser()
-parser.add_argument('--image')
+parser.add_argument('--images', nargs='+')
 args=parser.parse_args()
 
 # 定义人脸检测及性别分类所需的模型
@@ -53,23 +57,40 @@ genderList=['Male','Female']
 faceNet=cv2.dnn.readNet(faceModel,faceProto)
 genderNet=cv2.dnn.readNet(genderModel,genderProto)
 
-# 读取指定的图片
-frame = cv2.imread(args.image)
+result = []
 
-# 调用highlightFace函数进行人脸检测，返回绘制人脸框后的图像及人脸框位置信息
-resultImg,faceBoxes=highlightFace(faceNet,frame)
-# 如果未检测到人脸，输出提示信息
-if not faceBoxes:
-    print("No face detected")
+# 读取多张图片并进行处理
+for filename in args.images:
+    # 读取指定的图片
+    frame = cv2.imread(filename)
 
-# 如果检测到人脸，分别对每张人脸进行性别分类，并输出性别信息
-for faceBox in faceBoxes:
-    face=frame[max(0,faceBox[1]-20):
-               min(faceBox[3]+20,frame.shape[0]-1),max(0,faceBox[0]-20)
-               :min(faceBox[2]+20, frame.shape[1]-1)]
+    # 调用highlightFace函数进行人脸检测，返回绘制人脸框后的图像及人脸框位置信息
+    resultImg,faceBoxes=highlightFace(faceNet,frame)
 
-    blob=cv2.dnn.blobFromImage(face, 1.0, (227,227), [78.4263377603, 87.7689143744, 114.895847746], swapRB=False)
-    genderNet.setInput(blob)
-    genderPreds=genderNet.forward()
-    gender=genderList[genderPreds[0].argmax()]
-    print(f'Gender: {gender}')
+    # 构造输出的json对象
+    output = {"image": filename, "status": 0, "message": "", "gender": []}
+
+    # 如果未检测到人脸，输出提示信息
+    if not faceBoxes:
+        output["status"] = 1
+        output["message"] = "No face detected in the image"
+    else:
+        # 如果检测到人脸，分别对每张人脸进行性别分类，并输出性别信息
+        for faceBox in faceBoxes:
+            face=frame[max(0,faceBox[1]-20):
+                       min(faceBox[3]+20,frame.shape[0]-1),max(0,faceBox[0]-20)
+                       :min(faceBox[2]+20, frame.shape[1]-1)]
+
+            blob=cv2.dnn.blobFromImage(face, 1.0, (227,227), [78.4263377603, 87.7689143744, 114.895847746], swapRB=False)
+            genderNet.setInput(blob)
+            genderPreds=genderNet.forward()
+            gender=genderList[genderPreds[0].argmax()]
+
+            # 将每张脸的性别记录在output对象中
+            output["gender"].append(gender)
+
+    result.append(output)
+
+# 将json对象转为字符串输出
+# 输出的结果是一个 json 数组，包含每张图片中检测到的人脸信息，包括图片名称、状态码、状态信息和每个性别。如果未检测到人脸，状态码为 1，同时 message 字段提示没有检测到人脸
+print(json.dumps(result))
